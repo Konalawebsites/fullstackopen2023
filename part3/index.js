888a69086be3b2b6188425b56bfe52926c8fc763
Konalawebsites <atfitcad@gmail.com>
2023-09-20 12:11:28 +0300

    part3 serveri nettiin

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 morgan.token('body', req => { return JSON.stringify(req.body) })
 
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -89,6 +92,8 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
+
+  
 console.log(`Server running on port ${PORT}`)
