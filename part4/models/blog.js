0c82587bde47074bad6c248c13305a95f13142f6
Konalawebsites <atfitcad@gmail.com>
2023-09-27 14:12:58 +0300

    part4 moduulit jaettu osiin

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,28 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const password = "VXeYB3qiBmmzX85I"
-
-const url =
-  `mongodb+srv://fullStackAleksi:${password}@clusterfullstack.8ju3irb.mongodb.net/blogList?retryWrites=true&w=majority`
-
-console.log('connecting to', url)
-mongoose.connect(url)
-
-.then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
     url: String,
     likes: Number
   })
+
 
 blogSchema.set('toJSON', {
     transform: (document, returnedObject) => {
