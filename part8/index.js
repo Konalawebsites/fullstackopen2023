5f2093c91df7493c79be8a69ba667746d99bd779
Konalawebsites <atfitcad@gmail.com>
2024-01-16 18:32:02 +0200

    8.2 done

--- part8/index.js
+++ part8/index.js
@@ -113,6 +113,7 @@ type Author {
 type Query {
   bookCount: Int!
   authorCount: Int!
+  allBooks: [Book!]!
 }
 `
 
@@ -120,6 +121,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books
   },
   Book: {
     title: (root) => root.title,
